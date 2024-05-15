e1d91c288ec479f83a298826ebbbeb29b4e2c8e6
Allexwnl <151912971+Allexwnl@users.noreply.github.com>
2024-05-15 10:20:01 +0200

    Update yeet.lua

--- yeet.lua
+++ yeet.lua
@@ -1,24 +1,21 @@
 function print_inventory_on_monitor(chest, monitor)
-    
     local function writeLine(text)
         monitor.write(text)  -- Schrijf de tekst op de huidige regel
         _, y = monitor.getCursorPos()
         monitor.setCursorPos(1,y + 1)  -- Verplaats de cursor naar de volgende regel
     end
-    
-    
-        monitor.clear()  -- Clear the monitor screen
-        monitor.setCursorPos(1, 1)  -- Reset cursor position to top left corner
+
+    chest = peripheral.find("minecraft:chest")
     for slot, item in pairs(chest.list()) do
         local item_name = string.sub(item.name, 11)
-        writeLine(string.format("%dx %s\n", item.count, item_name))
+        writeLine(string.format("%dx %s", item.count, item_name))
     end
 end
 
 -- De while loop om elke 10 seconden de monitor te updaten
 while true do
     local monitor = peripheral.find("monitor")
-    local chest = peripheral.find("chest")
+    local chest = peripheral.find("minecraft:chest")
 
     print_inventory_on_monitor(chest, monitor)
     sleep(10)  -- Wait for 10 seconds before the next update
