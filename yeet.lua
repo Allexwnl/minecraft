8dbd9a7975d0cca2d6330cfd12b6bb8548a035ac
Allexwnl <151912971+Allexwnl@users.noreply.github.com>
2024-05-15 10:58:25 +0200

    Update yeet.lua

--- yeet.lua
+++ yeet.lua
@@ -1,10 +1,14 @@
-function print_inventory_on_monitor(chest, monitor)
-    local function writeLine(text)
-        monitor.write(text)  -- Schrijf de tekst op de huidige regel
-        _, y = monitor.getCursorPos()
-        monitor.setCursorPos(1,y + 1)  -- Verplaats de cursor naar de volgende regel
-    end
+local monitor = peripheral.find("monitor")
 
+
+local function writeLine(text)
+    monitor.clear()
+    monitor.write(text)  -- Schrijf de tekst op de huidige regel
+    _, y = monitor.getCursorPos()
+    monitor.setCursorPos(1,y + 1)  -- Verplaats de cursor naar de volgende regel
+end
+
+function print_inventory_on_monitor(chest, monitor)
     chest = peripheral.find("minecraft:chest")
     for slot, item in pairs(chest.list()) do
         local item_name = string.sub(item.name, 11)
